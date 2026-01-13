bcfaed37d7899abf827aeaabb84d2c99ac9ae0ff
gpollast <gpollast@student.42.fr>
2026-01-13 18:11:15 +0100

    (feat): ex01 beginning

---
+++ ex01/includes/Fixed.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gpollast <gpollast@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/13 11:37:32 by gpollast          #+#    #+#             */
+/*   Updated: 2026/01/13 16:58:18 by gpollast         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+class Fixed {
+private:
+    int					_fixed;
+	static const int	_fractionnal = 8;
+
+public:
+	Fixed();
+	Fixed(const Fixed& copy);
+	Fixed& operator=(const Fixed& copy);
+	~Fixed();
+	int getRawBits( void ) const;
+	void setRawBits( int const raw );
+};
